ff9e1dd1659e00153c6de1b90598da3cb50d2ac3
Tyorna <francescaarata@live.it>
2023-05-03 16:38:22 +0200

    es

---
+++ settimana02/lezione02/esercizio01/assets/js/script.js
@@ -0,0 +1,7 @@
+const prodotto = [
+    {
+    titolo: "Titolo",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, assumenda non! Eum quis blanditiis a eligendi veritatis aperiam minus quidem ab! Harum vitae sit id dolorem ipsa facere officia voluptate!",
+    
+}
+]
